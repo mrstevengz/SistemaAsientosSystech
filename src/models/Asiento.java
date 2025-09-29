41d5ece93016815e33c5e4d4c77a4a20c868293d
Fergago20 <fergomez@uamv.edu.ni>
2025-09-29 17:51:42 -0600

    Clase Asiento

--- src/models/Asiento.java
+++ src/models/Asiento.java
@@ -1,4 +1,24 @@
 package models;
 
 public class Asiento {
+
+    private int idAsiento;
+    private  boolean estado = false;
+    private int numero;
+    private Estudiante estudiante;
+
+    public Asiento(int idAsiento, int numero) {
+        this.idAsiento = idAsiento;
+        this.numero = numero;
+    }
+
+    public void asignar(Estudiante estudiante) {
+
+        if (!estado) {
+            this.estudiante = estudiante;
+            this.estado = true;
+        } else {
+            System.out.println("Asiento ya ocupado");
+        }
+    }
 }
