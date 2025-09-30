d2ad0e7f8ba0c009ec72b761d75cde1442e21310
gague-UAM <ggpaiz@uamv.edu.ni>
2025-09-29 20:26:51 -0600

    La clase estudiante se actualiza

--- src/models/Estudiante.java
+++ src/models/Estudiante.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Estudiante {
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private int cif;
+    private int numRecibo;
+
+    
+
+    public void registrarse(){
+
+    }
 }
