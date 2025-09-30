e8d4d4a41ee5dc35db77a7c4c78d690a6dcf3c0d
allnrvao <aracuna@uamv.edu.ni>
2025-09-29 20:39:18 -0600

    clase servicio

--- src/models/ServicioComida.java
+++ src/models/ServicioComida.java
@@ -1,4 +1,23 @@
 package models;
 
 public class ServicioComida {
+
+    private int idServicio;
+    private int turno;
+    private String rangoCredencial;
+
+    public ServicioComida(int idServicio, int turno, String rangoCredencial) {
+        this.idServicio = idServicio;
+        this.turno = turno;
+        this.rangoCredencial = rangoCredencial;
+    }
+
+
+    public void asignarTurno(Credencial credencial) {
+        System.out.println("Servicio de comida asignado al estudiante " + credencial.getNombre() + " en el turno " + turno);
+    }
+
+    public void atender(Credencial credencial) {
+        System.out.println("Atendiendo al estudiante " + credencial.getNombre() + " en el turno " + turno);
+    }
 }
